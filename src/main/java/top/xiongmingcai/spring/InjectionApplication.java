15fd9207a86d2ae3debea42d1181680b9c095798
xmc <xmc000@icloud.com>
2021-05-12 00:57:42 +0800

    使用 UserService 服务

--- src/main/java/top/xiongmingcai/spring/InjectionApplication.java
+++ src/main/java/top/xiongmingcai/spring/InjectionApplication.java
@@ -2,11 +2,13 @@ package top.xiongmingcai.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import top.xiongmingcai.spring.injection.service.UserService;
 
 public class InjectionApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:injection.xml");
-
+        UserService userService = (UserService) ctx.getBean("UserService");
+        userService.createUser();
     }
 }
 
