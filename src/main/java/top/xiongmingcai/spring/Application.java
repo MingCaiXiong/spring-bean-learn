1da480a681caf4101956b8dc22f3342c4fb548b9
xmc <xmc000@icloud.com>
2021-05-12 00:56:04 +0800

    使用setter方法运行时注入：（使用频率最高）

--- src/main/java/top/xiongmingcai/spring/Application.java
+++ src/main/java/top/xiongmingcai/spring/Application.java
@@ -2,9 +2,12 @@ package top.xiongmingcai.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import top.xiongmingcai.spring.injection.service.UserService;
 
 public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        UserService userService = (UserService) ctx.getBean("userService");
+        userService.createUser();
     }
 }
