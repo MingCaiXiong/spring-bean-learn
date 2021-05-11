cc0f7d4652455f8725164dc06b90ad1dd9637e29
xmc <xmc000@icloud.com>
2021-05-11 23:28:16 +0800

    标准项目初始化

---
+++ src/main/java/top/xiongmingcai/spring/Application.java
@@ -0,0 +1,10 @@
+package top.xiongmingcai.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+    }
+}
